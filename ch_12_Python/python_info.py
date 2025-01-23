50d814e106ed42bd98de7932140cf171d16ad69e
antonio-st@yandex.ru <antonio-st@yandex.ru>
2025-01-23 22:40:30 +0300

    save commit 20250123 (python ref)

--- ch_12_Python/python_info.py
+++ ch_12_Python/python_info.py
@@ -8,6 +8,7 @@ print(w_4[::-1]) # развернуть строку
 
 # считывание данных
 num1, num2, num3 = int(input()), int(input()), int(input())
+stage1, stage2, stage3 = [int(input()) for _ in range(3)]
 
 # округление
 round(sum(numbers1) / len(numbers1), 1)
@@ -53,6 +54,11 @@ list_to_str = " ".join(str_to_list[::-1])
 # список в строку 2
 *list_to_str
 
+#  проверка большой буквы
+any(1 for i in pass_check if i.isupper() is True)
+
+# проверка чиселки в пароле
+is_dig = any(1 for i in pass_check if i.isdigit() is True)
 
 # ============================ if ===============================
 
@@ -74,3 +80,17 @@ x = 7
 y = 12
 if (x > 5 and x < 10) or y == 12:
     print("Либо x в диапазоне от 5 до 10, либо y равно 12.")
+
+
+
+any(i < 50 for i in lst)
+
+
+if len([i for i in hours if i > 90]) >= 1
+
+# сортировка списка
+sorted([a, b, c]
+
+# проверка спец символов / двойной цикл в list compr
+list_char = ["!", "@", "#", "$", "%", "&", "*"]
+chk_char = 1 if [1 for i in pass_check for j in list_char if i in j] else 0
